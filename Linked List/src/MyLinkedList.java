4273cb47ccd48d6343d5dd8751ad4616853c45bf
John Anselmo <jjohnanselmo@gmail.com>
2018-10-21 20:42:47 -0400

    Oh god I forgot to commit

--- Linked List/src/MyLinkedList.java
+++ Linked List/src/MyLinkedList.java
@@ -22,7 +22,8 @@ public class MyLinkedList<E> {
     /**
      * Constructs a MyLinkedList with a collection
      * 
-     * @param c the collection to construct the linkedlist with
+     * @param c the collection to construct the 
+     * linkedlist with
      */
     public MyLinkedList( Collection<E> c ) {
         for ( E e : c ) {
@@ -106,13 +107,13 @@ public class MyLinkedList<E> {
        temp.next = temp.next.next;
        size--;
        return rem;
-       // 1 -> 2 -> 3 -> 4 -> 5
    }
     
     /**
      * returns the element at index index
      * @return the element at the specified index
-     * @throws IndexOutOfBoundsException when index < 0 || index >= size
+     * @throws IndexOutOfBoundsException when 
+     * index < 0 || index >= size
      */
     public E get( int index ) {
         if ( index < 0 || index >= size ) {
@@ -130,12 +131,152 @@ public class MyLinkedList<E> {
         add( index, obj );
         return toReturn;
     }
+    
+    /**
+     * gives the size of the linkedlist
+     * @return the amount of elements in the linkedlist
+     */
+    public int size() {
+        return size;
+    }
+    
+    /**
+     * tells whether or not the specified element is 
+     * in the linkedlist
+     * @param o the object that may or may not be in
+     * the list
+     * @return whether o is in the list or not
+     */
+    public boolean contains( Object o ) {
+        Node<E> temp = head;
+        while ( temp != null ) {
+            if ( temp.data.equals( o ) ) {
+                return true;
+            }
+            temp = temp.next;
+        }
+        return false;
+    }
+    
+    /**
+     * says if the list is empty
+     * @return if the list contains 0 elements
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    
+    /**
+     * adds all elements in the collection to the linkedlist
+     * @param c the collection of elements to add
+     */
+    public void addAll( Collection<E> c ) {
+        for ( E e : c ) {
+            add( e );
+        }
+    }
+    
+    /**
+     * clears the linkedlist
+     */
+    public void clear() {
+        head = null;
+        size = 0;
+    }
+    
+    /**
+     * returns the index of the first occurrence of the 
+     * specified object
+     * @param o the object to find the index of
+     * @return the object's first index
+     */
+    public int indexOf( Object o ) {
+        int index = 0;
+        for ( Node<E> temp = head; temp != null; temp = temp.next ) {
+            if ( temp.data.equals( o ) ) {
+                return index;
+            }
+            index++;
+        }
+        return -1;
+    }
+    
+    /**
+     * returns the last occurrence's index
+     * @param o the object to find the last index of
+     * @return the last index of the object
+     */
+    public int lastIndexOf( Object o ) {
+        int index = 0;
+        int curr = 0;
+        for ( Node<E> temp = head; temp != null; temp = temp.next ) {
+            if ( temp.data.equals( o ) ) {
+                index = curr;
+            }
+            curr++;
+        }
+        return index;
+    }
+    
+    /**
+     * removes all elements between the specified
+     * indices non inclusive
+     * @param from the first element to remove
+     * @param to the index after the last element to
+     * remove
+     */
+    public void removeRange( int from, int to ) {
+        for ( int i = to - 1; i >= from; i-- ) {
+            remove( i );
+        }
+    }
+    
+    /**
+     * returns an array version of the linkedlist
+     * @return an array of the linkedlist elements
+     */
+    public Object[] toArray() {
+        Object[] toReturn = new Object[ size ];
+        int index = 0;
+        for ( Node<E> temp = head; temp != null; temp = temp.next ) {
+            toReturn[ index ] = ( Object ) temp.data;
+            index++;
+        }
+        return toReturn;
+    }
+    
+    /**
+     * returns if the object is equal to the linkedlist
+     * @return if the object is equal to the linkedlist
+     */
+    public boolean equals( Object o ) {
+        if ( o == this ) {
+            return true;
+        }
+        if ( ! (o instanceof MyLinkedList ) ) {
+            return false;
+        }
+        MyLinkedList other = ( MyLinkedList ) o;
+        if ( size() == other.size() ) {
+            for (int i = 0; i < size; i++ ) {
+                if ( get( i ) != other.get( i ) ) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
 
     /**
-     * Used to create a visual of the linked list ( readable )
+     * Used to create a visual of the linked list 
+     * ( readable )
      * @return a string version of the linkedlist
      */
     public String toString() {
+        if ( size == 0 ) {
+            return "[]";
+        }
         String toReturn = "[";
         Node<E> current = head;
         while ( current.next != null ) {
@@ -160,8 +301,10 @@ public class MyLinkedList<E> {
         /**
          * Constructs a node with data and next node
          * 
-         * @param data the data to set the node's data to
-         * @param next the node that this new node looks to
+         * @param data the data to set the node's
+         * data to
+         * @param next the node that this new node 
+         * looks to
          */
         public Node( E data, Node<E> next ) {
             this.next = next;
@@ -170,7 +313,8 @@ public class MyLinkedList<E> {
         }
 
         /**
-         * constructs a new node with just data and next as null
+         * constructs a new node with just data and 
+         * next as null
          * 
          * @param data the data to set the node to
          */
--- Linked List/src/MyLinkedListTest.java
+++ Linked List/src/MyLinkedListTest.java
@@ -1,4 +1,10 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 import org.junit.After;
 import org.junit.Before;
@@ -11,6 +17,12 @@ import org.junit.Test;
  */
 public class MyLinkedListTest {
 	private MyLinkedList<Integer> regular;
+	private MyLinkedList<Integer> empty;
+	private MyLinkedList<Integer> repeat;
+	private MyLinkedList<Integer> regEquals;
+	
+	private Collection<Integer> addAll;
+	
 	
     
 	/**
@@ -19,8 +31,19 @@ public class MyLinkedListTest {
 	@Before
 	public void setUp() {
         regular = new MyLinkedList<Integer>();
+        addAll = new ArrayList<Integer>();
         for ( Integer i = 1; i < 6; i++ ) {
             regular.add( i );
+            addAll.add( i );
+            regEquals.add( i );
+        }
+        
+        empty = new MyLinkedList<Integer>();
+        
+        repeat = new MyLinkedList<Integer>();
+        for ( Integer i = 1; i < 6; i++ ) {
+            repeat.add( i );
+            repeat.add( i );
         }
         
     }
@@ -31,6 +54,7 @@ public class MyLinkedListTest {
 	@After
     public void tearDown() {
         regular = null;
+        empty = null;
     }
     
     // add( E e ) tests
@@ -112,9 +136,126 @@ public class MyLinkedListTest {
     /**
      * tests
      */
-//    @Test
-//    public void testSetRegular() {
-//        regular.set( 2, 9 );
-//        assertEquals( "[1, 2, 9, 4, 5]", regular.toString() );
-//    }
+    @Test
+    public void testSetRegular() {
+        regular.set( 2, 9 );
+        assertEquals( "[1, 2, 9, 4, 5]", regular.toString() );
+    }
+    
+    // size() tests
+    /**
+     * tests
+     */
+    @Test
+    public void testSizeReg() {
+        assertEquals( 5, regular.size() );
+    }
+    
+    // contains( Object o ) tests
+    /**
+     * tests
+     */
+    @Test
+    public void testContainsReg() {
+        assertTrue( regular.contains( 3 ) );
+    }
+    
+    /**
+     * tests
+     */
+    @Test
+    public void testNoContainsReg() {
+        assertFalse( regular.contains( 123 ) );
+    }
+    
+    // isEmpty() tests
+    /**
+     * tests
+     */
+    @Test
+    public void testIsEmptyReg() {
+        assertFalse( regular.isEmpty() );
+    }
+    
+    /**
+     * tests
+     */
+    @Test
+    public void testIsEmptyIs() {
+        assertTrue( empty.isEmpty() );
+    }
+    
+    // addAll( Collection<E> c ) tests
+    /**
+     * tests
+     */
+    @Test
+    public void testAddAllRegToReg() {
+        regular.addAll( addAll );
+        assertEquals( "[1, 2, 3, 4, 5, 1, 2, 3, 4, 5]", regular.toString() );
+    }
+    
+    // clear() tests
+    /**
+     * tests
+     */
+    @Test
+    public void testClearReg() {
+        regular.clear();
+        assertEquals( "[]", regular.toString() );
+    }
+    
+    // indexOf( Object o ) tests
+    /**
+     * tests
+     */
+    @Test
+    public void testIndexOfReg() {
+        assertEquals( 2, regular.indexOf( 3 ) );
+    }
+    
+    // lastIndexOf( Object o ) tests
+    /**
+     * tests
+     */
+    @Test
+    public void testLastIndexOfRepeat() {
+        assertEquals( 5, repeat.lastIndexOf( 3 ) );
+    }
+    
+    // removeRange( int from, int to ) tests
+    /**
+     * tests
+     */
+    @Test
+    public void testRemoveRangeRepeat() {
+        repeat.removeRange( 3,  6 );
+        // [1, 1, 2, 2, 3, 3, 4, 4, 5, 5]
+        assertEquals( "[1, 1, 2, 4, 4, 5, 5]", repeat.toString() );
+    }
+    
+    // toArray() tests
+    /**
+     * tests
+     */
+    @Test
+    public void testRegToArray() {
+        Object[] testing = { 1, 2, 3, 4, 5 };
+        assertArrayEquals( testing, regular.toArray() );
+    }
+    
+    // equals( Object o ) tests
+    /**
+     * tests
+     */
+    @Test
+    public void testRegNotEqual() {
+        assertFalse( regular.equals( repeat ) );
+    }
+    
+    /**
+     * tests
+     */
+    @Test
+    public void testRegEqual() {
 }
