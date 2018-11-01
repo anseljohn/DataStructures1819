6772d5f40d5543e1f815b4034f4ef07dbecc8961
John Anselmo <jjohnanselmo@gmail.com>
2018-11-01 19:11:04 -0400

    MyTree initial javadocs

--- Trees/src/MyTree.java
+++ Trees/src/MyTree.java
@@ -1,4 +1,16 @@
+/**
+ * Rewritten version of a Tree
+ * @author John Anselmo
+ * @version 1.0
+ */
 public class MyTree {
 
-    
+    /**
+     * TreeNode class used in MyTree
+     * @author John Anselmo
+     * @version 1.0
+     */
+    private class TreeNode {
+
+    }
 }
