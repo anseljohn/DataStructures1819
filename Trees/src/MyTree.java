4d2214c92661e3c479811a6572e13a1667417383
John Anselmo <jjohnanselmo@gmail.com>
2018-11-01 19:15:29 -0400

    Finish TreeNode class

--- Trees/src/MyTree.java
+++ Trees/src/MyTree.java
@@ -3,14 +3,28 @@
  * @author John Anselmo
  * @version 1.0
  */
-public class MyTree {
+public class MyTree<E> {
 
     /**
      * TreeNode class used in MyTree
      * @author John Anselmo
      * @version 1.0
      */
-    private class TreeNode {
+    private class TreeNode<E> {
+        private E data;
+        private TreeNode<E> left;
+        private TreeNode<E> right;
 
+        public TreeNode( E data ) {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+
+        public TreeNode( E data, TreeNode<E> left, TreeNode<E> right ) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
     }
 }
