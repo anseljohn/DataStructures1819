986823ef7ffec13dc005e8a17fddc6323482ca9b
John Anselmo <jjohnanselmo@gmail.com>
2018-12-21 15:39:20 -0500

    Basic project structure

---
+++ XMLFileValidator/XMLFileValidator.java
@@ -0,0 +1,35 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class XMLFileValidator {
+
+    public static void main( String[] args ) {
+        validate( "example.xml" );
+    }
+
+    public static void validate( String p ) {
+        ArrayList<String> lines = new ArrayList<>();
+        File f = new File( p );
+
+        try {
+            Scanner sc = new Scanner( f );
+            while ( sc.hasNext() ) {
+                lines.add( sc.nextLine() );
+            }
+        } catch ( Exception e ) {
+            System.out.println( e );
+        }
+        for ( int i = lines.size() - 1; i >= 0; i-- ) {
+            
+        }
+        for ( String line : lines ) {
+            checkLine( line, lines.indexOf( line ) + 1 );
+        }
+    }
+
+    public static void checkLine( String line, int lineNumber ) {
+        if ( lineNumber == 0 ) {}
+    }
+}
