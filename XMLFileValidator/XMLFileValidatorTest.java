719fb187ece07ba6f0d8797c72f276b3d7a67e03
John Anselmo <jjohnanselmo@gmail.com>
2019-01-22 12:27:05 -0500

    Validator tests

---
+++ XMLFileValidator/XMLFileValidatorTest.java
@@ -0,0 +1,21 @@
+import java.org.junit.*;
+
+import jdk.internal.jline.internal.TestAccessible;
+
+public class XMLFileValidatorTest {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
+    @Test
+    public void testXML() {
+        
+    }
+}
