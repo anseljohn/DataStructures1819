9a1b64e392515d764947423d0f677d31e25afdca
John Anselmo <jjohnanselmo@gmail.com>
2019-05-12 19:15:58 -0400

    Merging with git practice (from school)

--- Mapfoot/Edge.java
+++ Mapfoot/Edge.java
@@ -1,8 +1,54 @@
-public class Edge  {
+import greenfoot.*;
+
+public class Edge {
     private Location locOne;
     private Location locTwo;
+    private boolean isShown;
+
     public Edge(Location l1, Location l2) {
         locOne = l1;
+        l1.addEdge(this);
         locTwo = l2;
+        l2.addEdge(this);
+        isShown = false;
+    }
+    
+    public void show(World w) 
+    {
+        GreenfootImage bg = w.getBackground();
+        bg.setColor( Color.ORANGE );
+        bg.drawLine( locOne.getX(), locOne.getY(), 
+                     locTwo.getX(), locTwo.getY() );
+        isShown = true;
+    }
+    
+    public double getDistance() {
+        return Math.sqrt((Math.pow(locTwo.getX() - locOne.getX(), 2) + Math.pow(locTwo.getY() - locOne.getY(), 2)));
+    }
+    
+    public int compareTo(Edge e) {
+        return (int) (this.getDistance() - e.getDistance());
+    }
+    
+    public Location getLocOne() {
+        return locOne;
+    }
+    
+    public Location getLocTwo() {
+        return locTwo;
+    }
+
+    public boolean isShown() {
+        return isShown;
+    }
+
+    public Location getOppositeSide(Location l) {
+        if (getLocOne().equals(l)) {
+            return getLocTwo();
+        } else if (getLocTwo().equals(l)){
+            return getLocOne();
+        } else {
+            return null;
+        }
     }
 }
--- Mapfoot/Location.java
+++ Mapfoot/Location.java
@@ -1,13 +1,27 @@
 import greenfoot.*;
-import java.util.Objects;
+import java.util.*;
 
 public class Location extends Actor
 {
-    public int x;
-    public int y;
+    private int x;
+    private int y;
+    private boolean permanent;
+    private double distance;
+    private Location closest;
+
+    private ArrayList<Edge> connectedEdges;
+    
+    public Location(int x, int y) {
+        this.x = x;
+        this.y = y;
+        permanent = false;
+        distance = Integer.MAX_VALUE;
+        connectedEdges = new ArrayList<>();
+    }
+    
     public void act() 
     {
-        
+
     }   
     
     public boolean equals(Location l) {
@@ -20,5 +34,57 @@ public class Location extends Actor
     
     public String toString() {
         return "(" + x + ", " + y + ")";
+    }
+
+    public void addEdge(Edge e) {
+        connectedEdges.add(e);
+    }
+
+    public ArrayList<Edge> getEdges() {
+        return connectedEdges;
+    }
+
+    public ArrayList<Location> getConnections() {
+        ArrayList<Location> toReturn = new ArrayList<>();
+        for (Edge e : getEdges()) {
+            if (e.getLocOne().equals(this)) {
+                toReturn.add(e.getLocTwo());
+            } else {
+                toReturn.add(e.getLocOne());
+            }
+        }
+        return toReturn;
+    }
+    
+    public int getX() {
+        return x;
+    }
+    
+    public int getY() {
+        return y;
+    }
+
+    public boolean isPermanent() {
+        return permanent;
+    }
+
+    public void setPermanent() {
+        permanent = true;
+    }
+
+    public Location getClosest() {
+        return closest;
+    }
+
+    public void setClosest(Location l) {
+        closest = l;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double newDistance) {
+        distance = newDistance;
     }
 }
