20564ce88f943ce227fcf6730458dbc0bab48849
John Anselmo <AnselmJA20@pwcs-edu.org>
2019-05-13 10:45:20 -0400

    Person B task 2

---
+++ Git/Task2/src/PersonB.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class PersonB {
+	
+	public static void main(String[] args) {
+		Scanner sc = old scanner(System.out);
+		String name = sc.next();
+		StringBuilder sb = new StringBuilder(name);
+		System.out.println(sb.reverse());
+	}
+	
+}
