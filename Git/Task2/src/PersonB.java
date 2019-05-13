54fc1b695fe42f79ac2f101bb563ac3ff200d78f
Kayla Tucker <TuckerKA20@pwcs-edu.org>
2019-05-13 11:38:51 -0400

    Used a try/catch and changed a few words

--- Git/Task2/src/PersonB.java
+++ Git/Task2/src/PersonB.java
@@ -3,10 +3,26 @@ import java.util.Scanner;
 public class PersonB {
 	
 	public static void main(String[] args) {
-		Scanner sc = old scanner(System.out);
-		String name = sc.next();
-		StringBuilder sb = new StringBuilder(name);
-		System.out.println(sb.reverse());
+		
+		Scanner sc = new scanner(System.in);
+
+		try
+		{
+			String name = sc.nextLine();
+			StringBuilder sb = new StringBuilder(name);
+			System.out.println(sb.reverse());
+		}
+		catch ( Exception e )
+		{
+			System.out.print( e );
+		}
+		finally
+		{
+			if ( sc != null )
+			{
+				sc.close();
+			}
+		}
 	}
 	
 }
